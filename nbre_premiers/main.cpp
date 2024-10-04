b5f8af5b8e035c3b2767d17d288228ae53866f2b
Samin Mohiuddi <samin.mohiuddi@hes-so.ch>
2024-10-04 14:34:30 +0200

    version complet du code

--- nbre_premiers/main.cpp
+++ nbre_premiers/main.cpp
@@ -2,37 +2,60 @@
 #include <cstdlib>
 using namespace std;
 
-int main()
-{
+int main() {
 
-    int a;          //valeur donné par l'utilisateur
-    char b;         //valeur pour savoir si l'utilisateur recommence le programme
-
-
-    cout << "Ce programme ... " << endl;
+    //Variables utiles
+    int val;                //valeur donné par l'utilisateur
+    int colonne = 0;        //compteur pour revenir à la ligne après 5 nombres
+    char reponse;           //valeur pour savoir si l'utilisateur recommence le programme
+    bool restart = true;    //flag pour le relancement du programme
 
     do {
-        cout << "Entrer une valeur [2-1000] : ";
-        cin >> a;
-    } while (a < 2 or a > 1000);
+        if (restart) {
+            cout << "Ce programme ... " << endl;
 
-    for (int i = 2; i <= a; ++i) {
+            do {
+                cout << "Entrer une valeur [2-1000] : ";
+                cin >> val;
+            } while (val < 2 or val > 1000);
 
-        bool est_premier = true;
+            //Code nombre premier
+            for (int i = 2; i <= val; ++i) {
+                bool est_premier = true;
 
-        for (int j = 2; j * j <= i; ++j) {
-            if (i % j == 0) {
-                est_premier = false;
-                break;
+                for (int j = 2; j * j <= i; ++j) {
+
+                    if (i % j == 0) {
+                        est_premier = false;
+                        break;
+                    }
+                }
+
+                //code cinq nombres par lignes :
+                if (est_premier) {
+                    cout << '\t' << i << " ";
+                    ++colonne;
+
+                    if (colonne % 5 == 0) {
+                        cout << endl;
+                        colonne = 0;
+                    }
+                }
             }
         }
 
-        if (est_premier) {
-            cout << '\t' << i << " ";
+        cout << endl << "Voulez-vous recommencer [O/N] : " << endl;
+        cin >> reponse;
+
+        //Si oui le flag devient vrai et on recommence le programme
+        if (reponse == 'O') {
+            restart = true;
         }
-    }
+        else {
+            restart = false;
+        }
 
-
+    } while (reponse != 'N');
 
     return EXIT_SUCCESS;
 
